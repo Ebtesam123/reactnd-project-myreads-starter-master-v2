129adfd529c413499698c5ec9931c065fa215d5f
Ebtesam Shemis <82912529+Ebtesam123@users.noreply.github.com>
2021-07-30 04:26:05 +0200

    Updated version2

--- src/Book.js
+++ src/Book.js
@@ -1,34 +1,48 @@
+import React from "react";
 
-import React from 'react'
-
-class Book extends React.Component
-{
-    render(){
-        let b=this.props.bks;
-        return(
-            <li>
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, 
-                height: 193, 
-                backgroundImage: `url(${b.imageLinks ? b.imageLinks.thumbnail:''})`}}>
-                </div>
-              <div className="book-shelf-changer">
-                <select value={b.shelf} onChange={s=>this.props.changeShelve(s.target.value, b)}>
-                  <option value="move" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
-              </div>
+class Book extends React.Component {
+  state = {
+    books: [],
+  };
+  UpdateShelf = (event) => {
+    this.props.changeShelve(event.target.value, this.props.bks);
+  };
+  render() {
+    let b = this.props.bks;
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url(${
+                  b.imageLinks ? b.imageLinks.thumbnail : ""
+                })`,
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select value={b.shelf} onChange={this.UpdateShelf}>
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
             </div>
-            <div className="book-title">{b.title}</div>
-            <div className="book-authors">{b.authors? b.authors.join(', '):' '}</div>
           </div>
-        </li>
-        )
-    }
+          <div className="book-title">{b.title}</div>
+          <div className="book-authors">
+            {b.authors ? b.authors.join(", ") : " "}
+          </div>
+        </div>
+      </li>
+    );
+  }
 }
 
 export default Book;
